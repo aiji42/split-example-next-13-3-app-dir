f651e15076c8e8788ff55b9022a073be9ff725ba
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-04-11 15:01:24 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-const withSplit = require('next-with-split').withSplit({})
+const withSplit = require('next-with-split').withSplit({
+  splits: {
+    test1: {
+      path: '/*',
+      hosts: {
+        original: 'split-example-next-13-3-app-dir.vercel.app',
+        'split-test-1': 'split-example-next-13-3-app-dir-git-split-test-1-aiji42.vercel.app',
+      },
+    }
+  }
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
